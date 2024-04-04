ed3de6867d3912ca73f6274f3b24fb9cce781c97
Praveen-Rajes <praveen.20220268@iit.ac.lk>
2024-04-04 11:59:50 +0530

    updating script

--- Data_preparation.py
+++ Data_preparation.py
@@ -19,3 +19,23 @@ df.head()
 print(df['Location'].unique())
 
 # %%
+c = 0
+d = 0
+n = 0
+
+for i in df['Location']:
+    if i == 'Colombo Proper':
+        c += 1
+    if i == 'Deniyaya, Matara':
+        d += 1
+    if i == 'Nuwara Eliya Proper':
+        n += 1
+
+
+    
+
+print("c" ,c)
+print("d" ,d)
+print("n" ,n)
+# %%
+
